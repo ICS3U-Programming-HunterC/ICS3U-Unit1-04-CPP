28dcc666cf7a131b24aa48b5bdcdd6c6f36cef55
Ubuntu <ubuntu@ip-172-31-3-2.ec2.internal>
2022-02-25 02:05:07 +0000

    added basicMath.cpp and areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Hunter Connolly All rights reserved
+//
+// Created by: Hunter Connolly
+// February 24, 2022
+// This is program calculates the area and perimeter of a rectangle
+//     with dimensions 10cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "10cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "The Area is: " << (10*3) << "cm^2" << std::endl;
+    std::cout << "The Perimeter is: " << (2*(10+3)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Hunter Connolly All rights reserved
+//
+// Created by: Hunter Connolly
+// February 24, 2022
+// This is program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "5+6=" << (5+6) << std::endl;
+    std::cout << "6-3=" << (6-3) << std::endl;
+    std::cout << "18/3=" << (18/3) << std::endl;
+    std::cout << "5+3^3=" << (5+pow(3, 3)) << std::endl;
+}
